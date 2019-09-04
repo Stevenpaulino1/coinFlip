18138f321f5ebead77770116437502f84c84c6c1
Steven Paulino <stevenpaulino1@hotmail.com>
2019-09-03 23:40:39 -0400

    pre deployment

--- src/CoinFace.js
+++ src/CoinFace.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+import "./Coin.css";
+
 const coinFace = props => {
   console.log("coinface props", props);
   return (
-    <div>
-      <h1>{props.coinFace ? "Tails" : "Heads"}</h1>
+    <div className="Coin">
+      <img
+        src={
+          props.coinFace
+            ? "https://etc.usf.edu/clipart/40200/40215/hdoll_back_40215_md.gif"
+            : "https://www.wpclipart.com/money/US_Currency/US_Half_Dollar_front.png"
+        }
+      />
       <h3>Total Flips: {props.numHeads + props.numTails}</h3>
       <h4>Total Heads: {props.numHeads}</h4>
       <h4>Total Tails: {props.numTails}</h4>
